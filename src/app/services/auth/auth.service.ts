42219065cb69f278419fda3c9d3c042e731bdde0
Eduardo Monteiro de Andrade <edu22snt@gmail.com>
2026-05-14 15:07:11 -0300

    Ajustes

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
+import { environment } from '../../core/environments/environment';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  private api = 'http://localhost:8080';
-
-  constructor(private http: HttpClient) { }
+  protected domain: string | undefined;
+  
+  constructor(private http: HttpClient) {
+    this.domain = environment.domain;
+  }
 
   login ( username: string, password: string ) {
-    return this.http.post<any>(`${this.api}/api/authenticate`, { username, password }).pipe(
+    return this.http.post<any>(`${this.domain}/api/authenticate`, { username, password }).pipe(
       tap(response => {
         this.saveToken(response.jwt);
       })
@@ -33,5 +36,26 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('jwt');
+  }
+
+  getDecodedToken(): any {
+    const jwt = this.getToken();
+    if (!jwt) {
+      return null;
+    }
+    return jwtDecode(jwt);
+  }
+
+  getRoles(): string[] {
+    const decoded = this.getDecodedToken();
+    return decoded?.roles || [];
+  }
+
+  isAdmin(): boolean {
+    return this.getRoles().includes('ROLE_ADMIN');
+  }
+
+  isUser(): boolean {
+    return this.getRoles().includes('ROLE_USER');
   }
 }
